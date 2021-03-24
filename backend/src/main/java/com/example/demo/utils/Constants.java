cbaabfc78de593ba6a00a499d8ae0d71aa11be6f
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-24 21:05:11 +0100

    exception handler i utisl klase

---
+++ backend/src/main/java/com/example/demo/utils/Constants.java
@@ -0,0 +1,30 @@
+package com.example.demo.utils;
+
+public class Constants {
+
+	public static final String BACKEND_URL = "http://localhost:8080";	
+	public static final String FRONTEND_URL = "http://localhost:4200";	
+	public static final String KNOWLEDGE_GROUP = "knowledge";
+	public static final String KNOWLEDGE_ATRIFACT = "knowledge";
+	public static final String KNOWLEDGE_VERSION = "0.0.1-SNAPSHOT";
+	public static final String INITIAL_PASSWORD = "init password";
+	public static final double INITIAL_BALANCE = 1000000.0;
+	
+	public static final String CREATE_RULES = "create-rules";
+	public static final String INCREASE_RULES = "increase-rules";
+	public static final String RENEW_RULES = "renew-rules";
+	public static final String CLOSE_RULES = "close-rules";
+	
+	public static final String REPORT_RULES = "report-rules";
+	public static final String BASE_REPORT = "base-report";
+	public static final String REPORT1 = "report-1";
+	public static final String REPORT2 = "report-2";
+	public static final String REPORT3 = "report-3";
+	public static final String EVENTS_RULES_REALTIME = "events-rules-realtime";
+	public static final String EVENTS_RULES_PSEUDO = "events-rules-pseudo";
+
+	public static final String ENABLE_HEADER = "Access-Control-Expose-Headers";
+	public static final String FIRST_PAGE_HEADER = "First-Page";
+	public static final String LAST_PAGE_HEADER = "Last-Page";
+
+}
