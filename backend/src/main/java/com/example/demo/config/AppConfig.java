e70c06750b3f9f1dd910793c9e80fcd2b01aeeaf
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-24 22:02:10 +0100

    testno podesavanje kiecontainera, da bi radilo bez koriscenja lokalnog jara

--- backend/src/main/java/com/example/demo/config/AppConfig.java
+++ backend/src/main/java/com/example/demo/config/AppConfig.java
@@ -1,14 +1,11 @@
 package com.example.demo.config;
 
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.example.demo.utils.Constants;
 
 @Configuration
 @EnableAsync
@@ -17,11 +14,12 @@ public class AppConfig {
 
 	@Bean
 	public KieContainer kieContainer() {
-		KieServices kieService = KieServices.Factory.get();
-		KieContainer kieContainer = kieService.newKieContainer(kieService.newReleaseId(Constants.KNOWLEDGE_GROUP, Constants.KNOWLEDGE_ATRIFACT, Constants.KNOWLEDGE_VERSION));
-		KieScanner kieScanner = kieService.newKieScanner(kieContainer);
-		kieScanner.start(1000);
-		return kieContainer;
+		//KieServices kieService = KieServices.Factory.get();
+		//KieContainer kieContainer = kieService.newKieContainer(kieService.newReleaseId(Constants.KNOWLEDGE_GROUP, Constants.KNOWLEDGE_ATRIFACT, Constants.KNOWLEDGE_VERSION));
+		//KieScanner kieScanner = kieService.newKieScanner(kieContainer);
+		//kieScanner.start(1000);
+		//return kieContainer;
+		return KieServices.Factory.get().getKieClasspathContainer();
 	}
 	
 }
